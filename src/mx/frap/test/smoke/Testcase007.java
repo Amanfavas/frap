c82025f58a80188b60adb6303d776f188321464d
Amanfavas <31921883+Amanfavas@users.noreply.github.com>
2018-08-13 11:50:03 +0530

    Updated

--- src/mx/frap/test/smoke/Testcase007.java
+++ src/mx/frap/test/smoke/Testcase007.java
@@ -7,14 +7,20 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import mx.frap.test.config.TestConfig;
 import mx.frap.ui.object.LoginPage;
+import 
 public class TestCase007 {
  WebDriver driver;
+	Location location;
+	DropDownHandlers dropown;
 	LoginPage subscribe_btn;
 	@BeforeMethod
 	public void preCondition()
 	{
 		driver = TestConfig.getDriverInstance();
+		
+		location=new Location(driver);
 		subscribe_btn =  new LoginPage(driver);
+		dropdown =new  DropDownHandler();
 	}
 	@AfterMethod
 	public void postCondition()
@@ -28,7 +34,9 @@ public class TestCase007 {
 	login.click();
         subscribe_btn.getLoginButton().click();
         String actualEmptytext=subscribe_btn.getUsernameTextbox().getAttribute("value");
-         System.out.println(actualEmptytext);       
+         System.out.println(actualEmptytext);    
+	 dropdown.selectDDLByText(location.searchTextBox,"Bangalore")
         }
         }
+        
 }
